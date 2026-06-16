a641d9ab00d79e755fc4735967e50065ea4e7530
Elin Waring <elin.waring@gmail.com>
2026-06-16 18:34:47 -0400

    Update for new variables

--- R/accesstemplates.R
+++ R/accesstemplates.R
@@ -17,3 +17,22 @@ basicplots <- function(project = "icorp26"){
 
 }
 
+#' Create basic one variable plots
+#'
+#' Creates a file that matches the basic plots vignette
+#' @param project    The name of the folder to be used which
+#'                   is also the name of the project.
+#'                   By default, srop2026..
+#'
+#' @export
+assignment1 <- function(project = "icorp26"){
+     if (!file.exists("srop2026/rmarkdown/assignment1.Rmd")) {
+          rmarkdown::draft("srop2026/rmarkdown/assignment1.Rmd",
+                           template = "assignment1",
+                           package = "icorp26",
+                           edit = FALSE,
+          )
+     }
+
+}
+
