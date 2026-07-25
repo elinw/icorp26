f275dc4686119da73990d6957d9c926c9c452a2e
Elin Waring <elin.waring@gmail.com>
2026-07-25 16:30:30 -0400

    add more data and functions.

--- R/accesstemplates.R
+++ R/accesstemplates.R
@@ -6,15 +6,15 @@
 #'                   By default, srop2026..
 #'
 #' @export
-basicplots <- function(project = "icorp26"){
-     if (!file.exists("srop2026/rmarkdown/simpleplots.Rmd")) {
-          rmarkdown::draft("srop2026/rmarkdown/simpleplots.Rmd",
-                           template = "simpleplots",
-                           package = "icorp26",
-                           edit = FALSE,
-          )
-     }
-
+basicplots <- function(project = "icorp26") {
+  if (!file.exists("srop2026/rmarkdown/simpleplots.Rmd")) {
+    rmarkdown::draft(
+      "srop2026/rmarkdown/simpleplots.Rmd",
+      template = "simpleplots",
+      package = "icorp26",
+      edit = FALSE,
+    )
+  }
 }
 
 #' Create basic one variable plots
@@ -25,14 +25,30 @@ basicplots <- function(project = "icorp26"){
 #'                   By default, srop2026..
 #'
 #' @export
-assignment1 <- function(project = "icorp26"){
-     if (!file.exists("srop2026/rmarkdown/assignment1.Rmd")) {
-          rmarkdown::draft("srop2026/rmarkdown/assignment1.Rmd",
-                           template = "assignment1",
-                           package = "icorp26",
-                           edit = FALSE,
-          )
-     }
-
+assignment1 <- function(project = "icorp26") {
+  if (!file.exists("srop2026/rmarkdown/assignment1.Rmd")) {
+    rmarkdown::draft(
+      "srop2026/rmarkdown/assignment1.Rmd",
+      template = "assignment1",
+      package = "icorp26",
+      edit = FALSE,
+    )
+  }
 }
 
+#' create a copy of the ICORP pdf poster file
+#'
+#' Creates a folder and file with the skeleton file
+#' @param poster    The name of the folder to be used which
+#'                   is also the name of the file.
+#'
+#' @export
+posterpdf <- function(file) {
+  rmarkdown::draft(
+    file = file,
+    template = "posterdown_latex_icorp",
+    package = "posterdownplus",
+    create_dir = TRUE,
+    edit = FALSE
+  )
+}
