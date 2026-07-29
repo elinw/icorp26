c87ca163b19d0b434f0bbb3570f9ca7c1062edf4
Elin Waring <elin.waring@gmail.com>
2026-07-29 11:18:00 -0400

    Update resize_image

--- R/resize_image.R
+++ R/resize_image.R
@@ -6,11 +6,19 @@ resize_image <- function(
   type = c("profile", "qrcode")
 ) {
   image <- here(templatefolder, "images", image)
-  new_image <- load.image(here(image))
+
+  if (!file.exists(image)) {
+    stop(cat(image, " does not exist. Please check the name."))
+  }
+  new_image <- load.image(image)
   new_image <- imresize(new_image, size)
+
   if (type == "profile") {
-    save.image(new_image, here(templatefolder, "images", "profilenew.png"))
+    save.image(
+      new_image,
+      here::here(templatefolder, "images", "profilenew.png")
+    )
   } else {
-    save.image(new_image, here(templatefolder, "images", "qrcodenew.png"))
+    save.image(new_image, here::here(templatefolder, "images", "qrcodenew.png"))
   }
 }
